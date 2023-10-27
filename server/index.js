373e726617056c8aae7940ba064d21eff0e109c0
Javi <javiercamposcastello@gmail.com>
2023-10-26 20:10:39 -0500

    Added environment variable to select the port

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const { postAndProcessReceipt, getPointsByReceiptId } = require('./controllers/receipt');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
