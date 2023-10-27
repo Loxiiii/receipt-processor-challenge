a7f75803e5ec394052a8e0e312ff16927498d9bb
Javi <javiercamposcastello@gmail.com>
2023-10-26 19:51:11 -0500

    Added comments to the receipt controller

--- server/controllers/receipt.js
+++ server/controllers/receipt.js
@@ -1,8 +1,10 @@
 const uuid = require('uuid');
 const calculatePoints = require('../helpers/helpers');
 
+// This array is used to store the data in memory
 const receipts = [];
 
+// Controller to store a new receipt, returns the new uuid for the entered receipt
 const postAndProcessReceipt = (req, res) => {
   const existingReceipt = receipts.find(
     (receipt) => JSON.stringify(receipt.content) === JSON.stringify(req.body),
@@ -21,6 +23,7 @@ const postAndProcessReceipt = (req, res) => {
   }
 };
 
+// Controller to get the points of a receipt by ID
 const getPointsByReceiptId = (req, res) => {
   const requestedId = req.params.id;
   const requestedReceipt = receipts.find((receipt) => receipt.id === requestedId);
