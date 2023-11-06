bec320f92c5e4e614b54f43b85f084e7a71cd724
Javi <javiercamposcastello@gmail.com>
2023-11-05 18:31:26 -0600

    Removed / endpoint that returned Hello World!

--- server/index.js
+++ server/index.js
@@ -8,10 +8,6 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.post('/receipts/process', postAndProcessReceipt);
 
 app.get('/receipts/:id/points', getPointsByReceiptId);
